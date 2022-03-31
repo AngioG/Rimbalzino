f33a96ee3dd505a62c8598340594929f5eeb71f0
AngioG <72987164+AngioG@users.noreply.github.com>
2022-03-31 10:44:35 +0200

    Add files via upload

---
+++ Rimbalzino/Rimbalzino/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace Rimbalzino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btn_timer = new System.Windows.Forms.Button();
+            this.logs = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_minus = new System.Windows.Forms.Button();
+            this.txt_speed = new System.Windows.Forms.TextBox();
+            this.btn_plus = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1156, 58);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RIMBALZINO";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 93);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(799, 415);
+            this.panel1.TabIndex = 1;
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Enabled = false;
+            this.btn_stop.Location = new System.Drawing.Point(400, 58);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(200, 35);
+            this.btn_stop.TabIndex = 2;
+            this.btn_stop.Text = "FREEZE";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.StopAll);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(0, 58);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(200, 35);
+            this.btn_start.TabIndex = 3;
+            this.btn_start.Text = "START";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.Start);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Enabled = false;
+            this.btn_clear.Location = new System.Drawing.Point(599, 58);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(200, 37);
+            this.btn_clear.TabIndex = 4;
+            this.btn_clear.Text = "CLEAR";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.Clear);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btn_timer
+            // 
+            this.btn_timer.Enabled = false;
+            this.btn_timer.Location = new System.Drawing.Point(200, 58);
+            this.btn_timer.Name = "btn_timer";
+            this.btn_timer.Size = new System.Drawing.Size(200, 35);
+            this.btn_timer.TabIndex = 5;
+            this.btn_timer.Text = "STOP SPAWNING";
+            this.btn_timer.UseVisualStyleBackColor = true;
+            this.btn_timer.Click += new System.EventHandler(this.btn_timer_Click);
+            // 
+            // logs
+            // 
+            this.logs.FormattingEnabled = true;
+            this.logs.Location = new System.Drawing.Point(799, 101);
+            this.logs.Name = "logs";
+            this.logs.Size = new System.Drawing.Size(357, 407);
+            this.logs.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(806, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Velocità:";
+            // 
+            // btn_minus
+            // 
+            this.btn_minus.Enabled = false;
+            this.btn_minus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btn_minus.Location = new System.Drawing.Point(900, 50);
+            this.btn_minus.Name = "btn_minus";
+            this.btn_minus.Size = new System.Drawing.Size(30, 46);
+            this.btn_minus.TabIndex = 9;
+            this.btn_minus.Text = "-";
+            this.btn_minus.UseVisualStyleBackColor = true;
+            this.btn_minus.Click += new System.EventHandler(this.btn_minus_Click);
+            // 
+            // txt_speed
+            // 
+            this.txt_speed.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.txt_speed.Location = new System.Drawing.Point(930, 50);
+            this.txt_speed.Name = "txt_speed";
+            this.txt_speed.Size = new System.Drawing.Size(30, 45);
+            this.txt_speed.TabIndex = 10;
+            this.txt_speed.Text = "1";
+            this.txt_speed.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_plus
+            // 
+            this.btn_plus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btn_plus.Location = new System.Drawing.Point(960, 50);
+            this.btn_plus.Name = "btn_plus";
+            this.btn_plus.Size = new System.Drawing.Size(31, 46);
+            this.btn_plus.TabIndex = 11;
+            this.btn_plus.Text = "+";
+            this.btn_plus.UseVisualStyleBackColor = true;
+            this.btn_plus.Click += new System.EventHandler(this.btn_plus_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label3.Location = new System.Drawing.Point(1007, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 25);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Velocità:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1156, 508);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_plus);
+            this.Controls.Add(this.txt_speed);
+            this.Controls.Add(this.btn_minus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.logs);
+            this.Controls.Add(this.btn_timer);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btn_timer;
+        private System.Windows.Forms.ListBox logs;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_minus;
+        private System.Windows.Forms.TextBox txt_speed;
+        private System.Windows.Forms.Button btn_plus;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Rimbalzino/Rimbalzino/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rimbalzino
+{
+
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+
+        }
+
+        private void Start(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+                return;
+
+            timer1.Start();
+            logs.Items.Add(DateTime.Now.ToString("HH:mm:ss") + " - Ho fatto partire il timer");
+
+            foreach (var o in panel1.Controls)
+                (o as Sprite).Stop = false;
+
+            logs.Items.Add(DateTime.Now.ToString("HH:mm:ss") + " - Gli sprites hanno iniziato a muoversi");
+
+            var a = new Sprite("quadrato", panel1);
+            panel1.Controls.Add(a);
+            logs.Items.Add(DateTime.Now.ToString("HH:mm:ss") + $" - Ho generato un nuovo quadrato in posizione ({a.Location.X};{a.Location.Y})");
+
+            foreach (var o in panel1.Controls)
+                Task.Run(() => (o as Sprite).Run());
+
+            btn_start.Enabled = false;
+            btn_clear.Enabled = true;
+            btn_stop.Enabled = true;
+            btn_timer.Enabled = true;
+            
+
+            
+        }
+
+        private void StopAll(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled)
+                return;
+
+            timer1.Stop();
+            foreach (var o in panel1.Controls)
+                (o as Sprite).Stop = true;
+
+            logs.Items.Add(DateTime.Now.ToString("HH:mm:ss") + " - Ho fermato tutti gli sprites");
+
+            btn_start.Enabled = true;
+            btn_stop.Enabled = false;
+            btn_timer.Enabled = btn_stop.Enabled;           
+        }
+
+        private void Clear(object sender, EventArgs e)
+        {
+            bool running = timer1.Enabled;
+            timer1.Stop();
+            logs.Items.Add(DateTime.Now.ToString("HH:mm:ss") + " - Ho fermato la generazione degli sprites");
+
+            foreach (var o in panel1.Controls)
+                (o as Sprite).Stop = true;
+            logs.Items.Add(DateTime.Now.ToString("HH:mm:ss") + " - Ho fermato tutti gli sprites");
+
+            do
+            {
+                Control[] ele = new Control[panel1.Controls.Count];
+                int i = 0;
+                foreach (var o in panel1.Controls)
+                {
+                    ele[i] = o as Control;
+                    panel1.Controls.Remove(o as Control);
+                    i++;
+                }
+
+                for (int j = 0; j < i; j++)
+                    ele[j].Dispose();
+            } while (panel1.Controls.Count != 0);
+            logs.Items.Add(DateTime.Now.ToString("HH:mm:ss") + " - Ho eliminato tutti gli sprites");
+            if (running)
+                timer1.Start();
+            logs.Items.Add(DateTime.Now.ToString("HH:mm:ss") + " - Ho fatto ripartire la generazione degli sprites");
+
+            btn_start.Enabled = !running;
+            btn_clear.Enabled = false;
+            btn_stop.Enabled = running;
+            btn_timer.Enabled = running;
+        }
+
+        private void btn_timer_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            logs.Items.Add(DateTime.Now.ToString("HH:mm:ss") + " - Ho fermato la generazione degli sprites");
+
+            btn_start.Enabled = true;
+            btn_stop.Enabled = true;
+            btn_timer.Enabled = false;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var a = new Sprite("quadrato", panel1);
+            panel1.Controls.Add(a);
+            Task.Run(() => a.Run());
+            logs.Items.Add(DateTime.Now.ToString("HH:mm:ss") + $" - Ho generato un nuovo quadrato in posizione ({a.Location.X};{a.Location.Y})");
+
+            btn_clear.Enabled = true;
+        }
+
+        private void btn_plus_Click(object sender, EventArgs e)
+        {
+            var speed = int.Parse(txt_speed.Text);
+            speed += 1;
+            if (speed <= 5)
+                txt_speed.Text = speed.ToString();
+            if (speed == 5)
+                btn_plus.Enabled = false;
+            btn_minus.Enabled = true;
+
+            Sprite.Speed = speed;
+            logs.Items.Add(DateTime.Now.ToString("HH:mm:ss") + $" - Ho cambiato la velocità a {speed}");
+        }
+
+        private void btn_minus_Click(object sender, EventArgs e)
+        {
+            var speed = int.Parse(txt_speed.Text);
+            speed -= 1;
+            if (speed >= 1)
+                txt_speed.Text = speed.ToString();
+            if (speed == 1)
+                btn_minus.Enabled = false;
+            btn_plus.Enabled = true;
+
+            Sprite.Speed = speed;
+            logs.Items.Add(DateTime.Now.ToString("HH:mm:ss") + $" - Ho cambiato la velocità a {speed}");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Stop();
+            foreach (var o in panel1.Controls)
+                (o as Sprite).Stop = true;
+        }
+
+        private void Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        public class Sprite : PictureBox
+        {
+            #region static fields
+            public static int Speed = 1;
+            private static Random random = new Random();
+            private static Color[] colors = new Color[15];
+            private static bool firstInstance = true;
+            #endregion
+
+            #region attributes
+            private int bouncesLefts;
+            private bool nord = false;
+            private bool est = false;
+            private Control parent;
+            private bool toDispose = false;
+            public bool Stop = false;
+            #endregion
+
+            #region Constructor
+            public Sprite(string name, Control par)
+            {
+                if (firstInstance)
+                {
+                    colors[0] = Color.Red;
+                    colors[1] = Color.Blue;
+                    colors[2] = Color.Green;
+                    colors[3] = Color.Yellow;
+                    colors[4] = Color.Black;
+                    colors[5] = Color.Orange;
+                    colors[6] = Color.Purple;
+                    colors[7] = Color.Pink;
+                    colors[8] = Color.Chocolate;
+                    colors[9] = Color.Aqua;
+                    colors[10] = Color.Gold;
+                    colors[11] = Color.BlueViolet;
+                    colors[12] = Color.PaleVioletRed;
+                    colors[13] = Color.DarkOliveGreen;
+                    colors[14] = Color.SandyBrown;
+                }
+
+                base.Name = name;
+                parent = par;
+                int color = random.Next(0, 15);
+                base.BackColor = colors[color];
+                int size = random.Next(20, 81);
+                base.Location = new Point(random.Next(0, parent.Width - this.Width), random.Next(0, parent.Height - this.Height));
+                base.Size = new Size(size, size);
+                this.bouncesLefts = random.Next(2, 8);
+                nord = random.Next(0, 2) == 0 ? false: true;
+                est = random.Next(0, 2) == 0 ? false : true;
+
+                //Roba grafica
+                this.DoubleBuffered = true;
+                this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+                this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+                this.UpdateStyles();
+
+                this.OnClick += 
+            }
+            #endregion
+
+            #region Methods
+            public void Run()
+            {
+                parent = this.Parent;
+                while (!Stop && ! toDispose)
+                {
+                    this.Step();
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(15);
+                }
+
+                if (toDispose)
+                    this.Dispose(true);
+            }
+            private void Step()
+            {
+                bool bounced = false;
+                int y = this.nord ? -Speed : Speed;
+                int x = this.est ? -Speed : Speed;
+                Location = new Point(Location.X + x, Location.Y + y);
+
+                if(bouncesLefts != 0)
+                {
+                    if (Location.Y >= parent.Size.Height - Size.Height || Location.Y <= 0)
+                    {
+                        nord = !nord;
+                        bounced = true;
+                    }
+                    if (Location.X >= parent.Size.Width - Size.Width || Location.X <= 0)
+                    {
+                        est = !est;
+                        bounced = true;
+                    }
+
+                    if (bounced)
+                        bouncesLefts -= 1;
+                }
+
+                if (Location.Y <= -(Size.Width) || Location.Y >= parent.Size.Height + Size.Height || Location.X <= -(Size.Width) || Location.X >= parent.Size.Width + Size.Width)
+                    toDispose = true;
+
+            }
+
+
+            #endregion
+
+
+        }
+    }
+}
